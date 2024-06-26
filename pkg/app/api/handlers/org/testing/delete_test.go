9055e4fb9d8d306fd42d8f16d6cc36e4f3b8c613
Brad Clawsie <brad@b7j0c.org>
2024-06-26 17:22:44 -0500

    clean up org delete tests

--- pkg/app/api/handlers/org/testing/delete_test.go
+++ pkg/app/api/handlers/org/testing/delete_test.go
@@ -2,14 +2,11 @@ package testing
 
 import (
 	"context"
-	"encoding/json"
-	"io"
 	"net/http"
 	"net/url"
 
 	"github.com/grokloc/grokloc-apiserver/pkg/app"
 	"github.com/grokloc/grokloc-apiserver/pkg/app/admin/org"
-	"github.com/grokloc/grokloc-apiserver/pkg/app/api/handlers/token"
 	"github.com/grokloc/grokloc-apiserver/pkg/app/jwt"
 	"github.com/grokloc/grokloc-apiserver/pkg/app/models"
 	app_testing "github.com/grokloc/grokloc-apiserver/pkg/app/testing"
@@ -43,40 +40,12 @@ func (s *OrgSuite) TestDeleteAsRoot() {
 
 // TestDeleteAsOrgOwner demonstrates that org owner auth cannot update an org.
 func (s *OrgSuite) TestDeleteAsOrgOwner() {
-	conn, connErr := s.st.Master.Acquire(context.Background())
-	require.NoError(s.T(), connErr)
-	defer conn.Release()
-	o, owner, _, oErr := app_testing.TestOrgAndUser(conn.Conn(), s.st)
-	require.NoError(s.T(), oErr)
-	tokenReqUrl, tokenReqUrlErr := url.Parse(s.srv.URL + "/token")
-	require.NoError(s.T(), tokenReqUrlErr)
-	ownerTokenRequest := jwt.EncodeTokenRequest(owner.ID, owner.APISecret.String())
-	ownerReq := http.Request{
-		URL:    tokenReqUrl,
-		Method: http.MethodPost,
-		Header: map[string][]string{
-			app.IDHeader:           {owner.ID.String()},
-			app.TokenRequestHeader: {ownerTokenRequest},
-		},
-	}
-	resp, postErr := s.c.Do(&ownerReq)
-	require.NoError(s.T(), postErr)
-	require.Equal(s.T(), resp.StatusCode, http.StatusOK)
-	defer resp.Body.Close()
-	body, readErr := io.ReadAll(resp.Body)
-	require.NoError(s.T(), readErr)
-	var ownerTok token.JSONToken
-	umErr := json.Unmarshal(body, &ownerTok)
-	require.NoError(s.T(), umErr)
-	require.NotEmpty(s.T(), ownerTok.Token)
-
-	// try to set to inactive
-	u, urlErr := url.Parse(s.srv.URL + app.APIPath + s.st.APIVersion + "/org/" + o.ID.String())
+	u, urlErr := url.Parse(s.srv.URL + app.APIPath + s.st.APIVersion + "/org/" + s.o.ID.String())
 	require.NoError(s.T(), urlErr)
 	req, reqErr := http.NewRequest(http.MethodDelete, u.String(), nil)
 	require.NoError(s.T(), reqErr)
-	req.Header.Add(app.IDHeader, owner.ID.String())
-	req.Header.Add(app.AuthorizationHeader, jwt.SignedStringToHeaderValue(ownerTok.Token))
+	req.Header.Add(app.IDHeader, s.owner.ID.String())
+	req.Header.Add(app.AuthorizationHeader, jwt.SignedStringToHeaderValue(s.ownerTok.Token))
 	resp, deleteErr := s.c.Do(req)
 	require.NoError(s.T(), deleteErr)
 	require.Equal(s.T(), http.StatusForbidden, resp.StatusCode)
@@ -84,40 +53,12 @@ func (s *OrgSuite) TestDeleteAsOrgOwner() {
 
 // TestDeleteAsRegularUser demonstrates that user auth cannot update an org.
 func (s *OrgSuite) TestDeleteAsRegularUser() {
-	conn, connErr := s.st.Master.Acquire(context.Background())
-	require.NoError(s.T(), connErr)
-	defer conn.Release()
-	o, _, regularUser, oErr := app_testing.TestOrgAndUser(conn.Conn(), s.st)
-	require.NoError(s.T(), oErr)
-	tokenReqUrl, tokenReqUrlErr := url.Parse(s.srv.URL + "/token")
-	require.NoError(s.T(), tokenReqUrlErr)
-	regularUserTokenRequest := jwt.EncodeTokenRequest(regularUser.ID, regularUser.APISecret.String())
-	regularUserReq := http.Request{
-		URL:    tokenReqUrl,
-		Method: http.MethodPost,
-		Header: map[string][]string{
-			app.IDHeader:           {regularUser.ID.String()},
-			app.TokenRequestHeader: {regularUserTokenRequest},
-		},
-	}
-	resp, postErr := s.c.Do(&regularUserReq)
-	require.NoError(s.T(), postErr)
-	require.Equal(s.T(), resp.StatusCode, http.StatusOK)
-	defer resp.Body.Close()
-	body, readErr := io.ReadAll(resp.Body)
-	require.NoError(s.T(), readErr)
-	var regularUserTok token.JSONToken
-	umErr := json.Unmarshal(body, &regularUserTok)
-	require.NoError(s.T(), umErr)
-	require.NotEmpty(s.T(), regularUserTok.Token)
-
-	// try to set to inactive
-	u, urlErr := url.Parse(s.srv.URL + app.APIPath + s.st.APIVersion + "/org/" + o.ID.String())
+	u, urlErr := url.Parse(s.srv.URL + app.APIPath + s.st.APIVersion + "/org/" + s.o.ID.String())
 	require.NoError(s.T(), urlErr)
 	req, reqErr := http.NewRequest(http.MethodDelete, u.String(), nil)
 	require.NoError(s.T(), reqErr)
-	req.Header.Add(app.IDHeader, regularUser.ID.String())
-	req.Header.Add(app.AuthorizationHeader, jwt.SignedStringToHeaderValue(regularUserTok.Token))
+	req.Header.Add(app.IDHeader, s.regularUser.ID.String())
+	req.Header.Add(app.AuthorizationHeader, jwt.SignedStringToHeaderValue(s.regularUserTok.Token))
 	resp, deleteErr := s.c.Do(req)
 	require.NoError(s.T(), deleteErr)
 	require.Equal(s.T(), http.StatusForbidden, resp.StatusCode)
