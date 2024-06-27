d8d32d961694381c936409036a5eebe94dd5db82
Brad Clawsie <brad@b7j0c.org>
2024-06-27 13:38:07 -0500

    add untested user update client methods

--- pkg/app/api/client/user.go
+++ pkg/app/api/client/user.go
@@ -101,3 +101,171 @@ func (client *Client) ReadUser(id models.ID) (*user.User, error) {
 
 	return &u, nil
 }
+
+func (client *Client) UpdateUserAPISecret(id models.ID) (*user.User, error) {
+	updateUserUrl, updateUserUrlErr := url.Parse(client.apiUrl.String() + "/user/" + id.String())
+	if updateUserUrlErr != nil {
+		return nil, updateUserUrlErr
+	}
+
+	bs, bsErr := json.Marshal(user.UpdateAPISecretEvent{GenerateAPISecret: true})
+	if bsErr != nil {
+		return nil, bsErr
+	}
+
+	req, reqErr := http.NewRequest(http.MethodPut, updateUserUrl.String(), bytes.NewReader(bs))
+	if reqErr != nil {
+		return nil, reqErr
+	}
+
+	refreshErr := client.RefreshToken()
+	if refreshErr != nil {
+		return nil, refreshErr
+	}
+
+	req.Header.Add(app.IDHeader, client.id.String())
+	req.Header.Add(app.AuthorizationHeader, jwt.SignedStringToHeaderValue(client.token))
+	resp, respErr := client.c.Do(req)
+	if respErr != nil {
+		return nil, respErr
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, ResponseErr{StatusCode: resp.StatusCode}
+	}
+
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	var u user.User
+	decErr := dec.Decode(&u)
+	if decErr != nil {
+		return nil, decErr
+	}
+
+	return &u, nil
+}
+
+func (client *Client) UpdateUserDisplayName(id models.ID, displayName safe.VarChar) (*user.User, error) {
+	updateUserUrl, updateUserUrlErr := url.Parse(client.apiUrl.String() + "/user/" + id.String())
+	if updateUserUrlErr != nil {
+		return nil, updateUserUrlErr
+	}
+
+	bs, bsErr := json.Marshal(user.UpdateDisplayNameEvent{DisplayName: displayName})
+	if bsErr != nil {
+		return nil, bsErr
+	}
+
+	req, reqErr := http.NewRequest(http.MethodPut, updateUserUrl.String(), bytes.NewReader(bs))
+	if reqErr != nil {
+		return nil, reqErr
+	}
+
+	refreshErr := client.RefreshToken()
+	if refreshErr != nil {
+		return nil, refreshErr
+	}
+
+	req.Header.Add(app.IDHeader, client.id.String())
+	req.Header.Add(app.AuthorizationHeader, jwt.SignedStringToHeaderValue(client.token))
+	resp, respErr := client.c.Do(req)
+	if respErr != nil {
+		return nil, respErr
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, ResponseErr{StatusCode: resp.StatusCode}
+	}
+
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	var u user.User
+	decErr := dec.Decode(&u)
+	if decErr != nil {
+		return nil, decErr
+	}
+
+	return &u, nil
+}
+
+func (client *Client) UpdateUserPassword(id models.ID, password safe.Password) (*user.User, error) {
+	updateUserUrl, updateUserUrlErr := url.Parse(client.apiUrl.String() + "/user/" + id.String())
+	if updateUserUrlErr != nil {
+		return nil, updateUserUrlErr
+	}
+
+	bs, bsErr := json.Marshal(user.UpdatePasswordEvent{Password: password})
+	if bsErr != nil {
+		return nil, bsErr
+	}
+
+	req, reqErr := http.NewRequest(http.MethodPut, updateUserUrl.String(), bytes.NewReader(bs))
+	if reqErr != nil {
+		return nil, reqErr
+	}
+
+	refreshErr := client.RefreshToken()
+	if refreshErr != nil {
+		return nil, refreshErr
+	}
+
+	req.Header.Add(app.IDHeader, client.id.String())
+	req.Header.Add(app.AuthorizationHeader, jwt.SignedStringToHeaderValue(client.token))
+	resp, respErr := client.c.Do(req)
+	if respErr != nil {
+		return nil, respErr
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, ResponseErr{StatusCode: resp.StatusCode}
+	}
+
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	var u user.User
+	decErr := dec.Decode(&u)
+	if decErr != nil {
+		return nil, decErr
+	}
+
+	return &u, nil
+}
+
+func (client *Client) UpdateUserStatus(id models.ID, status models.Status) (*user.User, error) {
+	updateUserUrl, updateUserUrlErr := url.Parse(client.apiUrl.String() + "/user/" + id.String())
+	if updateUserUrlErr != nil {
+		return nil, updateUserUrlErr
+	}
+
+	bs, bsErr := json.Marshal(user.UpdateStatusEvent{Status: status})
+	if bsErr != nil {
+		return nil, bsErr
+	}
+
+	req, reqErr := http.NewRequest(http.MethodPut, updateUserUrl.String(), bytes.NewReader(bs))
+	if reqErr != nil {
+		return nil, reqErr
+	}
+
+	refreshErr := client.RefreshToken()
+	if refreshErr != nil {
+		return nil, refreshErr
+	}
+
+	req.Header.Add(app.IDHeader, client.id.String())
+	req.Header.Add(app.AuthorizationHeader, jwt.SignedStringToHeaderValue(client.token))
+	resp, respErr := client.c.Do(req)
+	if respErr != nil {
+		return nil, respErr
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, ResponseErr{StatusCode: resp.StatusCode}
+	}
+
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	var u user.User
+	decErr := dec.Decode(&u)
+	if decErr != nil {
+		return nil, decErr
+	}
+
+	return &u, nil
+}
